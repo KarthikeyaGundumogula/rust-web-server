03ebb766dd6536b3d7396dd8da48e7b5e8c95ec4
KarthikeyaGundumogula <karthikeya200226@gmail.com>
2025-12-31 00:15:48 +0530

    created a single threaded server

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+use std::thread::JoinHandle;
+
+pub struct ThreadPool;
+
+impl ThreadPool {
+    /// Create a new ThreadPool.
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is zero.
+
+  pub fn new(count:usize) -> Self{
+    assert!(count > 0);
+    ThreadPool
+  }
+
+  pub fn execute<F>(&self, f:F)
+  where F: FnOnce() + Send + 'static
+  {
+
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,41 @@
 use std::{
+    fs,
     io::{BufReader, prelude::*},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
+
+use web_server::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-
-        handle_connection(stream);
+        let pool = ThreadPool::new(4);
+        pool.execute(|| {
+            println!("creating new thread");
+            handle_connection(stream);
+        });
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let req = buf_reader.lines().next().unwrap().unwrap();
+    let (status_line, html_res) = match &req[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "home.html"),
+        "GET /wait HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            println!("handling wait");
+            ("HTTP/1.1 200 OK", "home.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "error.html"),
+    };
+    let contents = fs::read_to_string(html_res).unwrap();
+    let length = contents.len();
 
-    println!("Request: {http_request:#?}");
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    stream.write_all(response.as_bytes()).unwrap();
 }
